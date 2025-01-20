1dc684406f3b716f890c04dde048ca039cb361ad
sungyeon <24203738+YeoniH@users.noreply.github.com>
2025-01-20 16:03:13 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+TobbieII.rightward()
+TobbieII.leftward()
+TobbieII.rightward()
+if (input.buttonIsPressed(Button.A)) {
+    TobbieII.forward()
+} else if (input.buttonIsPressed(Button.B)) {
+    TobbieII.stopwalk()
+}
